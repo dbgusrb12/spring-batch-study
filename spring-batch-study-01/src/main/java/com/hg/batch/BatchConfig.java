36a3cb6640e4678aa3e2d6829db1108e772f7227
hyungyu <hg.yu@twolinecloud.com>
2025-06-27 11:13:09 +0900

    lint: lint 수정

--- spring-batch-study-01/src/main/java/com/hg/batch/BatchConfig.java
+++ spring-batch-study-01/src/main/java/com/hg/batch/BatchConfig.java
@@ -1,6 +1,5 @@
 package com.hg.batch;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -10,6 +9,8 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class BatchConfig {
@@ -27,52 +28,52 @@ public class BatchConfig {
     @Bean
     public Job helloBatchJob() {
         return new JobBuilder("helloBatchJob", jobRepository)
-            .start(enterBatchStep())
-            .next(greetBatchStep())
-            .next(loopBatchStep())
-            .next(completeBatchStep())
-            .build();
+                .start(enterBatchStep())
+                .next(greetBatchStep())
+                .next(loopBatchStep())
+                .next(completeBatchStep())
+                .build();
     }
 
     @Bean
     public Step enterBatchStep() {
         return new StepBuilder("enterBatchStep", jobRepository)
-            .tasklet((contribution, chunkContext) -> {
-                System.out.println("Enter Batch Step !!");
-                return RepeatStatus.FINISHED;
-            }, transactionManager).build();
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Enter Batch Step !!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
     }
 
     @Bean
     public Step greetBatchStep() {
         return new StepBuilder("greetBatchStep", jobRepository)
-            .tasklet((contribution, chunkContext) -> {
-                System.out.println("Greet Batch Step !!");
-                System.out.println("배치 시작하기 : " + LOOP_TARGET + "번의 루프를 도는 배치");
-                return RepeatStatus.FINISHED;
-            }, transactionManager).build();
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Greet Batch Step !!");
+                    System.out.println("배치 시작하기 : " + LOOP_TARGET + "번의 루프를 도는 배치");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
     }
 
     @Bean
     public Step loopBatchStep() {
         return new StepBuilder("loopBatchStep", jobRepository)
-            .tasklet((contribution, chunkContext) -> {
-                int currentLoopCount = this.loopCount.incrementAndGet();
-                System.out.println("루프 돌기 : 현재 : " + currentLoopCount + " / " + LOOP_TARGET);
-                if (currentLoopCount < LOOP_TARGET) {
-                    return RepeatStatus.CONTINUABLE;
-                }
-                return RepeatStatus.FINISHED;
-            }, transactionManager).build();
+                .tasklet((contribution, chunkContext) -> {
+                    int currentLoopCount = this.loopCount.incrementAndGet();
+                    System.out.println("루프 돌기 : 현재 : " + currentLoopCount + " / " + LOOP_TARGET);
+                    if (currentLoopCount < LOOP_TARGET) {
+                        return RepeatStatus.CONTINUABLE;
+                    }
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
     }
 
     @Bean
     public Step completeBatchStep() {
         return new StepBuilder("completeBatchStep", jobRepository)
-            .tasklet((contribution, chunkContext) -> {
-                System.out.println("Complete Batch Step !!");
-                System.out.println("배치 완료");
-                return RepeatStatus.FINISHED;
-            }, transactionManager).build();
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Complete Batch Step !!");
+                    System.out.println("배치 완료");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
     }
 }
